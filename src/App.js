463b1a13914d03524f8ab80477bb40fa25cef4ff
Guyatu1627 <guyatugd.com>
2024-08-27 20:55:11 +0300

    |

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './pages/About'
+import Blog from './pages/Blog'
+import Class from './pages/Class'
+import Contact from './pages/Contact'
+import Gallery from './pages/Gallery'
+import Home from './pages/Home'
+import Single from './pages/Single'
+import Team from './pages/Team'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/blog' element={<Blog />} />
+          <Route path='/class' element={<Class />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/Gallery' element={<Gallery />} />
+          <Route path='/single' element={<Single />} />
+          <Route path='/team' element={<Team />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
